d6effae39c3827963318caeaa440f085d6c60dd1
cuimeili <1179734754@qq.com>
2018-05-15 10:21:43 +0800

    2018-5-15 10:20:26
    提现须知文案修改

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,8 @@
 /**
  * 系统常量配置
+ * 第一行为好卖货小程序配置 第二行为南华汇小程序配置
  */
+// export let API_URL = 'https://api.haomaihuoshop.com'
 export let API_URL = 'https://api.huahuishang.com'
 // export let API_URL = 'http://apinhh.xf66.com'
 // export let API_URL = 'http://127.0.0.1'
@@ -9,7 +11,18 @@ export let API_SIGNATURE = 'NhH$ApI'
 
 export let DEFAULT_UID = 0
 
+// export let IMG_URL = 'https://wxassets.haomaihuoshop.com'
 export let IMG_URL = 'https://wxassets.huahuishang.com'
 // export let IMG_URL = 'http://wxassets.xf66.com'
 
+// export let STATIC_IMG_PATH = 'https://wxassets.haomaihuoshop.com/static/images'
 export let STATIC_IMG_PATH = 'https://huahuishang.com/static/images'
+
+// export let DEFAULT_URL = "https://haomaihuoshop.com"
+export let DEFAULT_URL = "https://huahuishang.com"
+
+// export let DEFAULT_TITLE = "好卖货"
+export let DEFAULT_TITLE = "我的环球精品惠"
+
+// export let DEFAULT_NAME = "好卖货"
+export let DEFAULT_NAME = "南华汇"
